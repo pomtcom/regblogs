91087aa1a3e2dec67a569dcacf08d87ec082f378
pomtcom <pomtcom@hotmail.com>
2018-01-02 22:09:23 +0700

    code for 2-Jan-18

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { BackendService } from '../../service/backend/backend.service' ;
 import { TOPICS } from '../topic';
 
@@ -7,12 +7,16 @@ import { TOPICS } from '../topic';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
+  encapsulation: ViewEncapsulation.None,
   providers: [BackendService]
 })
 export class HomeComponent implements OnInit {
 
   value: string = "";
   topics = TOPICS ;
+
+  calendar='<h2 class="demo">somedd title</h2><div>some text</div><h3>other title</h3>' ;
+  testHtml = '<div class="demo"><b>This is my HTMLxx.</b></div>';
 
   constructor(private _BackendService: BackendService) { }
 
--- src/app/topic.ts
+++ src/app/topic.ts
@@ -3,6 +3,7 @@ export class Topic {
     topicName: string;
     thumbnail: string;
     shortDescrp: string;
+    
 }
 
 export const TOPICS: Topic[] = [
@@ -10,19 +11,21 @@ export const TOPICS: Topic[] = [
         id: 1,
         topicName: "ทำงานมา 4 ปี ค้นพบอะไรบ้าง? ผมจะเล่าให้ฟัง",
         thumbnail: "../../assets/images/topic_1.jpg",
-        shortDescrp: "xxxxxxx"
+        shortDescrp: "เนื่องด้วยผมครบรอบการทำงานเป็นมนุษย์เงินเดือนมา 4 ปี วันนี้จึงอยากมาเล่าให้ทุกคนฟังครับ ว่าผมค้นพบ และเห็นสัจธรรมในชีวิตอะไรบ้าง",
+        // shortDescrp: '<p style="color:red;">TEST TOPICss is working or not</p>'
+        
     },
     {
         id: 2,
-        topicName: "มาสร้าง private bitcoin wallet กันเถอะ",
+        topicName: "มาสร้าง private bitcoin wallet ของตัวเองกันเถอะ",
         thumbnail: "../../assets/images/topic_2.jpg",
-        shortDescrp: "xxx"
+        shortDescrp: "จากราคา bitcoin ที่พุ่งขึ้นเอาๆ คงไม่ดีแน่ถ้าเรายังเก็บ bitcoin ของเราไว้ที่เว็ปเทรด"
     },
     {
         id: 3,
         topicName: "มาเรียน Golang กันเถอะ",
         thumbnail: "../../assets/images/topic_3.jpg",
-        shortDescrp: "xxx"
+        shortDescrp: "แนะนำภาษา Go programming เบื้องต้น"
     },
     {
         id: 4,
